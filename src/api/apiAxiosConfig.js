32c5b5e65d551f8d6d51d67147a8d296094aad7a
dev <dev@dev>
2025-02-09 02:54:01 +0000

    first commit001

--- src/api/apiAxiosConfig.js
+++ src/api/apiAxiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 // Configuration de l'URL de base de l'API depuis l'environnement
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://blogapi.up.railway.app/api';
 
 // Création d'une instance Axios avec des configurations par défaut
 const apiClient = axios.create({
